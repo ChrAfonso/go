420852cecc94a57788d706a385688127b9329302
Christian Afonso <chr.afonso@gmail.com>
2016-03-19 16:28:48 +0100

    Detect connected stones, liberties, remove dead groups. TODO: don't suicide, detect living groups, count territory

--- main.lua
+++ main.lua
@@ -2,6 +2,9 @@ EMPTY = 0
 BLACK = 1
 WHITE = 2
 
+drawLastMoveLiberties = true
+drawLastMoveConnectedStones = true
+
 function love.load()
   -- init
   boardsize = 5
@@ -41,8 +44,14 @@ function love.draw()
   end
 
   -- stones
-  for stone = 1,move-1 do
-    drawStone(moves[stone])
+  for i = 1,boardsize do
+    for j = 1,boardsize do
+      if board[i][j] ~= EMPTY then drawStone(makeStone(i, j, board[i][j])) end
+    end
+  end
+  
+  if lastMove ~= nil then
+    drawLastMove()
   end
 end
 
@@ -56,6 +65,27 @@ function drawStone(stone)
   love.graphics.circle("fill", x(stone.x), y(stone.y), padding/2)
 end
 
+function drawLastMove()
+  if lastMove == nil then return end
+
+  love.graphics.setColor(255, 0, 0)
+  love.graphics.circle("line", x(lastMove.stone.x), y(lastMove.stone.y), padding/3)
+
+  if drawLastMoveLiberties then
+    love.graphics.setColor(255, 255, 0)
+    for _,liberty in pairs(lastMove.liberties) do
+      love.graphics.circle("fill", x(liberty.x), y(liberty.y), padding/4)
+    end
+  end
+
+  if drawLastMoveConnectedStones then
+    love.graphics.setColor(255, 255, 0)
+    for _,stone in pairs(lastMove.connectedStones) do
+      love.graphics.circle("line", x(stone.x), y(stone.y), padding/2)
+    end
+  end
+end
+
 function x(i)
   return 2*padding + (i-1)*padding
 end
@@ -65,9 +95,7 @@ function y(j)
 end
 
 function love.keypressed(k,s,r)
-  if k == "space" then
-    step()
-  end
+  step()
 end
 
 function step()
@@ -99,9 +127,38 @@ function getFreePoints()
 end
 
 function placeStone(i, j, col)
-  table.insert(moves, {x=i,y=j,color=col})
-  board[i][j] = color
+  local s = makeStone(i, j, col)
+  table.insert(moves, s)
+  board[i][j] = col
   move = move + 1
+  
+  local connectedStones = getConnectedStones(i, j)
+  local liberties = getLiberties(connectedStones)
+  lastMove = {
+    stone = s,
+    liberties = liberties,
+    connectedStones = connectedStones
+  }
+  
+  -- DEBUG
+  print("Liberties at last stone: " .. #lastMove.liberties)
+
+  removeDeadGroups()
+end
+
+function removeDeadGroups()
+  -- TODO make less crude
+  for i = 1,boardsize do
+    for j = 1,boardsize do
+      local connectStones = getConnectedStones(i, j)
+      if getNumLiberties(connectStones) == 0 then
+        local color = board[i][j] -- TODO for counting
+        for _,stone in pairs(connectStones) do
+          board[stone.x][stone.y] = EMPTY
+        end
+      end
+    end
+  end
 end
 
 function getCurrentColor()
@@ -112,9 +169,86 @@ function getCurrentColor()
   end
 end
 
+function getFreePositions()
+  local free = 0
+  for i = 1,boardsize do
+    for j = 1,boardsize do
+      if board[i][j] == EMPTY then free = free + 1 end
+    end
+  end
+end
+
 function checkEnd()
-  if move > (boardsize*boardsize + 1) then
+  if getFreePoints() == 0 then
     os.exit()
   end
 end
 
+function getConnectedStones(i, j)
+  local stones = {makeStone(i, j, board[i][j])}
+  getConnectedStonesR(i, j, stones)
+  return stones
+end
+
+function getConnectedStonesR(i, j, stones)
+  for _,position in pairs(getSurroundingPositions(i, j)) do
+    local m,n = position[1], position[2]
+    if board[i][j] == board[m][n] and not isStoneInTable(m, n, stones) then
+      table.insert(stones, makeStone(m, n, board[m][n]))
+      getConnectedStonesR(m, n, stones)
+    end
+  end
+end
+
+function getSurroundingPositions(i, j)
+  local positions = {}
+  for _,position in pairs({{i-1,j},{i+1,j},{i,j-1},{i,j+1}}) do
+    if isPositionOnBoard(position[1], position[2]) then
+      table.insert(positions, position)
+    end
+  end
+  return positions 
+end
+
+function getNumLiberties(stones)
+  local liberties = getLiberties(stones)
+  return #liberties
+end
+
+function getLiberties(stones)
+  local liberties = {}
+  for _,stone in pairs(stones) do
+    collectLiberties(stone.x, stone.y, liberties)
+  end
+  return liberties
+end
+
+function collectLiberties(i, j, liberties)
+  for _,position in pairs(getSurroundingPositions(i, j)) do
+    if board[position[1]][position[2]] == EMPTY
+    and not isStoneInTable(position[1], position[2], liberties) then
+      table.insert(liberties, {x=position[1], y=position[2]})
+    end
+  end
+end
+
+function makeStone(i, j, col)
+  return {x=i, y=j, color=col}
+end
+
+function isStoneInTable(i, j, t)
+  for _,s in pairs(t) do
+    if s.x == i and s.y == j then
+      return true
+    end
+  end
+  return false
+end
+
+function isPositionOnBoard(i, j)
+  if (i >= 1) and (i <= boardsize) and (j >= 1) and (j <= boardsize) then
+    return true
+  else
+    return false
+  end
+end
